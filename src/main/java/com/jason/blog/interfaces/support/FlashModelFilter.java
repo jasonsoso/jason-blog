f3e4d420561f5437cf4a24dfdd7a0ced0bcbc868
jasonsoso <648636045@qq.com>
2012-12-29 01:45:39 +0800

    Filter Share data between two request

--- src/main/java/com/jason/blog/interfaces/support/FlashModelFilter.java
+++ src/main/java/com/jason/blog/interfaces/support/FlashModelFilter.java
@@ -12,20 +12,23 @@ import javax.servlet.http.HttpSession;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 
-
+/**
+ * Filter
+ * Share data between two request
+ * Mainly provide prompt information
+ * @author Jason
+ *
+ */
 public final class FlashModelFilter extends OncePerRequestFilter {
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.springframework.web.filter.OncePerRequestFilter#doFilterInternal(javax.servlet.http. HttpServletRequest,
-	 * javax.servlet.http.HttpServletResponse, javax.servlet.FilterChain)
+	
+	/* (non-Javadoc)
+	 * @see org.springframework.web.filter.OncePerRequestFilter#doFilterInternal(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, javax.servlet.FilterChain)
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-
+		
 		// how to share data between two request?
 		// the flashModelFilter try to do this
 		// we put first request'data in to the session
@@ -37,10 +40,13 @@ public final class FlashModelFilter extends OncePerRequestFilter {
 			if (null != flash) {
 				for (Map.Entry<String, ?> entry : flash.entrySet()) {
 					Object currentValue = request.getAttribute(entry.getKey());
-					if (null != currentValue) {
+					if (null == currentValue) {
+						//key is 'message' ,  value is 'type' or 'text'
+						//So EL expression is ${message.type} or ${message.text}
 						request.setAttribute(entry.getKey(), entry.getValue());
 					}
 				}
+				//clear the request 's data in session
 				session.removeAttribute(FlashModel.FLASH_MODEL_ATTRIBUTE);
 			}
 		}
